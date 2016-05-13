b3d045adfa778d7b2a2c1f17576f916783e339f3
David Kyalo <davidmkyalo@gmail.com>
2016-05-13 18:12:08 +0300

    Set inital version

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -9,9 +9,11 @@
 # the suggestion, or a newly entered value.
 
 # once the new version number is determined, the script will
-# pull a list of changes from git history, prepend this to
-# a file called CHANGES (under the title of the new version
+# pull a list of CHANGELOG.MD from git history, prepend this to
+# a file called CHANGELOG.MD (under the title of the new version
 # number) and create a GIT tag.
+
+today=$(date +"%F %R")
 
 if [ -f VERSION ]; then
     BASE_STRING=`cat VERSION`
@@ -29,13 +31,16 @@ if [ -f VERSION ]; then
     fi
     echo "Will set new version to be $INPUT_STRING"
     echo $INPUT_STRING > VERSION
-    echo "Version $INPUT_STRING:" > tmpfile
+    
+    echo "## Version $INPUT_STRING [$today]:" > tmpfile    
+    echo "" >> tmpfile
     git log --pretty=format:" - %s" "v$BASE_STRING"...HEAD >> tmpfile
     echo "" >> tmpfile
     echo "" >> tmpfile
-    cat CHANGES >> tmpfile
-    mv tmpfile CHANGES
-    git add CHANGES VERSION
+    echo "" >> tmpfile
+    cat CHANGELOG.MD >> tmpfile
+    mv tmpfile CHANGELOG.MD
+    git add CHANGELOG.MD VERSION
     git commit -m "Version bump to $INPUT_STRING"
     git tag -a -m "Tagging version $INPUT_STRING" "v$INPUT_STRING"
     git push origin --tags
@@ -49,12 +54,12 @@ else
     if [ "$RESPONSE" = "YES" ]; then RESPONSE="y"; fi
     if [ "$RESPONSE" = "y" ]; then
         echo "0.1.0" > VERSION
-        echo "Version 0.1.0" > CHANGES
-        git log --pretty=format:" - %s" >> CHANGES
-        echo "" >> CHANGES
-        echo "" >> CHANGES
-        git add VERSION CHANGES
-        git commit -m "Added VERSION and CHANGES files, Version bump to v0.1.0"
+        echo "Version 0.1.0" > CHANGELOG.MD
+        git log --pretty=format:" - %s" >> CHANGELOG.MD
+        echo "" >> CHANGELOG.MD
+        echo "" >> CHANGELOG.MD
+        git add VERSION CHANGELOG.MD
+        git commit -m "Added VERSION and CHANGELOG.MD files, Version bump to v0.1.0"
         git tag -a -m "Tagging version 0.1.0" "v0.1.0"
         git push origin --tags
     fi
